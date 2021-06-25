957dfdf9340d1114f87c46164cfa2351932dc8cb
jingdaxia <1174877839@qq.com>
2021-06-25 14:13:30 +0800

    初始化项目

---
+++ src/main/java/com/jcy/account/JaccountApplication.java
@@ -0,0 +1,13 @@
+package com.jcy.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaccountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jcy/account/JaccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcy.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
